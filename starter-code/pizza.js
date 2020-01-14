29785d8b01a99a2dd8f6a6356a54fc814e198996
NicolasPerezDLC <54360376+NicolasPerezDLC@users.noreply.github.com>
2020-01-14 09:40:52 -0500

    Update pizza.js

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -39,32 +39,59 @@ function renderPepperonni() {
     }
     else {
       $pep.style.visibility = "hidden";
-    }
+    } 
   })
 }
 
 function renderMushrooms() {
   // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(function($mushroom){
+    if (state.mushrooms) {
+      $mushroom.style.visibility = "visible";
+    }
+    else {
+      $mushroom.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
+  // document.querySelectorAll('.green-pepper').forEach(function($green-pepper){
+  //   if (state.green-pepper) {
+  //     $green-pepper.style.visibility = "visible";
+  //   }
+  //   else {
+  //     $green-pepper.style.visibility = "hidden";
+  //   }
+  // })
 }
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  // document.querySelectorAll('.pep').forEach(function($pep){
+  //   if (state.pepperonni) {
+  //     $pep.style.visibility = "visible";
+  //   }
+  //   else {
+  //     $pep.style.visibility = "hidden";
+  //   }
+  // })
 }
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+ 
 }
 
 function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  
 }
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
+  
 }
 
 
@@ -77,9 +104,26 @@ document.querySelector('.btn.btn-pepperonni').onclick = function() {
 }
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
+document.querySelector('.btn.btn-mushrooms').onclick = function() {
+  console.log ('test')
+  state.mushrooms = !state.mushrooms
+  renderEverything()
+}
 
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+// document.querySelector('.btn.btn-green-peppers').onclick = function() {
+//   state.green-peppers = !state.green-peppers
+//   renderEverything()
+// }
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+// document.querySelector('.btn.btn-sauce').onclick = function() {
+//   state.green-peppers = !state.green-peppers
+//   renderEverything()
+// }
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+// document.querySelector('.btn.btn-crust').onclick = function() {
+//   state.crust = !state.crust
+//   renderEverything()
+// }
